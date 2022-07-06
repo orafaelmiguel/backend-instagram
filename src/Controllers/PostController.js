2c508e0c25dea688ce9c49258c39610e9d08b15a
Rafael Miguel <rafaelmiguel0202@gmail.com>
2022-07-06 18:57:28 -0300

    create posts routes

---
+++ src/Controllers/PostController.js
@@ -0,0 +1,19 @@
+const Post = require('../Models/Post')
+
+module.exports = {
+    async createPost(req, res) {
+
+    },
+
+    async listAllPosts(req, res) {
+
+    },
+
+    async deletePost(req, res) {
+
+    },
+
+    async editPost(req, res) {
+        
+    }
+}
--- src/Utils/routes.js
+++ src/Utils/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express') // const router = require('express').Router()
 const LoginController = require('../Controllers/LoginController')
+const PostController = require('../Controllers/PostController')
 const UserController = require('../Controllers/UserController')
 
 const router = Router()
 
+// Rotas linkadas aos arquivos na posta Controllers
 // registro
 router.post('/users', UserController.createUser)
 router.get('/users', UserController.listUser)
@@ -11,6 +13,10 @@ router.get('/users', UserController.listUser)
 router.post('/login', LoginController.login)
 // logout
 // feed
+router.post('/posts', PostController.createPost)
+router.get('/posts', PostController.listAllPosts)
+router.delete('/post/:post_id', PostController.deletePost)
+router.put('/post/:post_id', PostController.editPost)
 // like
 // postar foto
 // deletar foto
