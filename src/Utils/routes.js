b72572dd92d12148c4a637e71fe902d30759ad15
Rafael Miguel <rafaelmiguel0202@gmail.com>
2022-07-06 12:59:37 -0300

    Update routes.js

--- src/Utils/routes.js
+++ src/Utils/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+const { Router } = require('express') // const router = require('express').Router()
 const UserController = require('../Controllers/UserController')
 
 const router = Router()
