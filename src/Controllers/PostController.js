a6cf6d45d9d1837fa93e01a0ef54f8aeb4b23d2c
Rafael Miguel <rafaelmiguel0202@gmail.com>
2022-07-06 21:57:05 -0300

    create post

--- src/Controllers/PostController.js
+++ src/Controllers/PostController.js
@@ -2,7 +2,27 @@ const Post = require('../Models/Post')
 
 module.exports = {
     async createPost(req, res) {
+        const {
+            picture,
+            description
+        } = req.body
 
+        const { user } = req.headers
+
+        try {
+            const newPost = await Post.create({
+                picture,
+                description,
+                user
+            })
+
+            return res.status(200).send({
+                message: 'Post created successfully',
+                data: newPost
+            })
+        } catch (error) {
+            res.status(400).send(error)
+        }
     },
 
     async listAllPosts(req, res) {
@@ -14,6 +34,6 @@ module.exports = {
     },
 
     async editPost(req, res) {
-        
+
     }
 }
