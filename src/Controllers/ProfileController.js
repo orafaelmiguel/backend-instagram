4cfb18b7aae208d2271b9fd686a9bfac60575f09
Rafael Miguel <rafaelmiguel0202@gmail.com>
2022-07-09 20:20:34 -0300

    list profile user

--- src/Controllers/ProfileController.js
+++ src/Controllers/ProfileController.js
@@ -1,4 +1,5 @@
 const User = require('../Models/User')
+const Post = require('../Models/Post')
 
 module.exports = {
     async getProfile(req,res) {
@@ -6,10 +7,14 @@ module.exports = {
 
         try {
             const userInfo = await User.findById(user_id)
+            if(!userInfo) return res.status(404).send('User does not exist')
+
+            const userPosts = await Post.find({ user: user_id })
 
             return res.status(200).send({ 
                 message: 'User found',
-                userInfo
+                userInfo,
+                userPosts
              })
         } catch (error) {
             return res.status(400).send(error)
