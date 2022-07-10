166f966abb7440a310443d586027f23ba57fbe91
Rafael Miguel <rafaelmiguel0202@gmail.com>
2022-07-10 14:36:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ const cors = require('cors')
 require('dotenv').config()
 
 const mongoConnection = process.env.MONGO_URI
+var port = process.env.PORT || 3333
 
 const app = express()
+app.use(cors)
+app.use(express.json())
+app.use(router)
+
 
 // conexão com banco de dados
 mongoose.connect(mongoConnection, {
@@ -15,13 +20,9 @@ mongoose.connect(mongoConnection, {
 })
 .then(() => {
     console.log('connected to database')
-    app.listen(process.env.PORT || 3000, () => {
+    app.listen(port, () => {
         console.log('Server running')
     })
 })
 .catch((error) => {console.log(error)})
-
-app.use(cors)
-app.use(express.json())
-app.use(router)
 
